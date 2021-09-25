16d4205aad87f23285b9c29bef1c4fcd689df823
Peter <70147953+Petee99@users.noreply.github.com>
2021-09-25 20:37:09 +0200

    Create form.php

---
+++ E8HZ8D_0907/form.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="style.css">
+
+<?php
+echo ("<strong>Név:</strong>" . $_POST['name'] . "<br><br>");
+echo ("<strong>PIN kód:</strong>" . $_POST['pin'] . "<br><br>");
+echo ("<strong>Gyümölcs:</strong>" . $_POST['fruit'] . "<br><br>");
+echo ("<strong>Életkor:</strong>" . $_POST['age'] . "<br><br>");
+echo ("<strong>Lábméret:</strong>" . $_POST['fsize'] . "<br><br>");
+echo ("<strong>Önbizalom:</strong>" . $_POST['confidence'] . "<br><br>");
+?>
+
+<a href="./index.html" class="previous">&laquo; Vissza</a>
